d74c7b3dadb1ca49e2412732af848a7c8c5e841a
David Norrish <norrish.d@gmail.com>
2017-11-26 17:56:52 +1100

    Add scatter plot example

---
+++ plotlyGraph.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Create a scatter plot from a Pandas dataframe using the dash_core_components
+Graph component.
+
+Graph renders interactive data visualizations using the open source plotly.js
+JavaScript graphing library. Supports over 35 chart types and renders charts in
+both vector-quality SVG and high-performance WebGL.
+
+Plotly documentation: https://plot.ly/python
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+app = dash.Dash()
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
+    'gdp-life-exp-2007.csv')
+
+
+# Inferences: line determines the outline of the 'markers'/circles
+# legend is listing the coordinates or something
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server()
