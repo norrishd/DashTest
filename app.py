ac0f2c3e0c779694689088e3e404e7d99feb2505
David Norrish <norrish.d@gmail.com>
2017-11-26 17:16:35 +1100

    Add CSS styles to tutorial piece

--- app.py
+++ app.py
@@ -6,36 +6,52 @@ My first ever Dash app, just following the tutorial at https://plot.ly/dash/gett
 import dash
 # Library with a component for every HTML tag
 import dash_html_components as html
-# Higher-level interactive components generated with JavaScript, HTML, and CSS through React.js
+# Higher-level interactive components with JS, HTML, and CSS through React.js
 import dash_core_components as dcc
 
 # Declare a new app
 app = dash.Dash()
 
-# Import default CSS styling
+# Import default CSS styling for font and a bunch of stuff
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+# A JSON-like dictionary to use as CSS style information
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
 # Define the layout, which is a tree of components:
 # a div which contains a heading, a div and a graph
 # Components are described entirely through their keyword attributes
-app.layout = html.Div(children=[
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     # Generates <h1>Wow, a Python web app!</h1>
-    # 'children' is always the first attribute; can omit: html.H1('Hello Dash')
-    html.H1(children='Wow, a Python web app!'),
-    
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    # 'children' is always 1st attribute; can omit, e.g. html.H1('Hello Dash')
+    html.H1(
+        children='Wow, a Python web app!',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Here is a test bar chart to appreciate.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Melbourne'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Canberra'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Australia East Coast Vis',
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': colors['text']
             }
         }
     )
@@ -43,4 +59,3 @@ app.layout = html.Div(children=[
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
